ce088de89c83846064620d48a9d43e0c3cf7cafa
RohitRaj766 <rohitraj2k04@gmail.com>
2024-06-05 09:39:24 +0530

    .

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
         <Navbar />
         <div>
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
